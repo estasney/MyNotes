881e16e4b38dea4de2762f95a1bc55f73b193d06
estasney <estasney@users.noreply.github.com>
2020-06-26 22:56:47 -0400

    add keyword model

--- mynotes/utils/storage/models/model.py
+++ mynotes/utils/storage/models/model.py
@@ -7,10 +7,40 @@ module_notebook = Table('module_notebook', Base.metadata,
                         Column('notebook_id', Integer, ForeignKey('notebooks.id'))
                         )
 
+keyword_notebook = Table('keyword_notebook', Base.metadata,
+                         Column('keyword_id', Integer, ForeignKey('keywords.id')),
+                         Column('notebook_id', Integer, ForeignKey('notebooks.id'))
+                         )
+
 module_category = Table('module_category', Base.metadata,
                         Column('module_id', Integer, ForeignKey('modules.id')),
                         Column('category_id', Integer, ForeignKey('categories.id'))
                         )
+
+keyword_category = Table('keyword_category', Base.metadata,
+                         Column('keyword_id', Integer, ForeignKey('keywords.id')),
+                         Column('category_id', Integer, ForeignKey('categories.id'))
+                         )
+
+
+class Keyword(Base):
+    __tablename__ = "keywords"
+
+    JSON_KEYS = ['id', 'name']
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    notebooks = relationship("Notebook",
+                             secondary=keyword_notebook,
+                             back_populates='keywords')
+    categories = relationship("Category",
+                              secondary=module_category,
+                              back_populates='modules')
+
+    def to_dict(self) -> dict:
+        d = {k: getattr(self, k, None) for k in self.JSON_KEYS}
+        d['table'] = self.__tablename__
+        return d
 
 
 class Module(Base):
@@ -46,6 +76,9 @@ class Notebook(Base):
     modules = relationship("Module",
                            secondary=module_notebook,
                            back_populates='notebooks')
+    keywords = relationship("Keyword",
+                            secondary=keyword_notebook,
+                            back_populates='notebooks')
     category_id = Column(Integer, ForeignKey('categories.id'))
     category = relationship("Category", back_populates='notebooks')
 
@@ -58,6 +91,7 @@ class Notebook(Base):
             url = "/" + self.name
         d['url'] = url
         d['modules'] = [m.to_dict() for m in self.modules]
+        d['keywords'] = [kw.to_dict() for kw in self.keywords]
         return d
 
 
@@ -76,6 +110,9 @@ class Category(Base):
     modules = relationship("Module",
                            secondary=module_category,
                            back_populates='categories')
+    keywords = relationship("Keyword",
+                            secondary=keyword_category,
+                            back_populates="categories")
 
     @property
     def url(self):
@@ -83,7 +120,6 @@ class Category(Base):
         if not parent:
             return self.name
         return parent.url + "/" + self.name
-
 
     def to_dict(self):
         d = {k: getattr(self, k, None) for k in self.JSON_KEYS}
@@ -93,4 +129,5 @@ class Category(Base):
         d['children'] = [c.to_dict() for c in self.children_categories]
         d['notebooks'] = [n.to_dict(self.url) for n in self.notebooks]
         d['modules'] = [m.to_dict() for m in self.modules]
+        d['keywords'] = [kw.to_dict() for kw in self.keywords]
         return d
